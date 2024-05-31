5048bf05370f0574b5592ace2c93dc1d5124d696
VulpoiuPetru <petru.vulpoiu@student.unitbv.ro>
2024-05-31 09:35:09 +0300

    Create the Supermarket

---
+++ Tema3-Magazin/Tema3.1/View/Login.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Tema3._1.View.Admin;
+using Tema3._1.View.Cashier;
+using Tema3._1.ViewModel;
+
+namespace Tema3._1.View
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+            (DataContext as UserVM).OnMoveToNextWindow += LogInView_OnMoveToNextWindow;
+        }
+        private void LogInView_OnMoveToNextWindow(object sender, bool isAdmin)
+        {
+            if (isAdmin)
+            {
+                AdminView adminView = new AdminView();
+                adminView.ShowDialog();
+            }
+            else
+            {
+                CashierView cashierView = new CashierView();
+                cashierView.ShowDialog();
+            }
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is UserVM viewModel)
+            {
+                viewModel.Password = ((PasswordBox)sender).SecurePassword.Copy();
+            }
+        }
+
+        
+    }
+}
---
+++ Tema3-Magazin/Tema3.1/View/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Tema3._1.ViewModel;
+using Tema3._1.View.Admin;
+using Tema3._1.View.Cashier;
+
+namespace Tema3._1.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+
+        }
+       
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            Register register;
+            register = new Register();
+            register.Show();
+            //this.Close();
+        }
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            Login login;
+            login = new Login();
+            login.Show();
+            //this.Close();
+        }
+    }
+}
